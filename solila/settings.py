b6914fb5bc53a6d0d7e0b17b0555c1bbd580841f
Stelian Iancu <stelian.iancu@gmail.com>
2022-03-04 18:11:29 +0100

    Text changes.

--- solila/settings.py
+++ solila/settings.py
@@ -10,7 +10,6 @@ DEBUG = os.environ.get("JUNTAGRICO_DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ['solila.juntagrico.science', 'localhost', 'juntagrico.solila-eulenhof.ch', '127.0.0.1']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -32,12 +31,14 @@ ROOT_URLCONF = 'solila.urls'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('JUNTAGRICO_DATABASE_ENGINE','django.db.backends.postgresql'),
-        'NAME': os.environ.get('JUNTAGRICO_DATABASE_NAME','juntagrico'),
-        'USER': os.environ.get('JUNTAGRICO_DATABASE_USER', 'solila'), #''junatagrico', # The following settings are not used with sqlite3:
-        'PASSWORD': os.environ.get('JUNTAGRICO_DATABASE_PASSWORD', 'solila'), #''junatagrico',
-        'HOST': os.environ.get('JUNTAGRICO_DATABASE_HOST', 'localhost'), #'localhost', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': os.environ.get('JUNTAGRICO_DATABASE_PORT', '5432'), #''', # Set to empty string for default.
+        'ENGINE': os.environ.get('JUNTAGRICO_DATABASE_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.environ.get('JUNTAGRICO_DATABASE_NAME', 'juntagrico'),
+        'USER': os.environ.get('JUNTAGRICO_DATABASE_USER', 'solila'),
+        # ''junatagrico', # The following settings are not used with sqlite3:
+        'PASSWORD': os.environ.get('JUNTAGRICO_DATABASE_PASSWORD', 'solila'),  # ''junatagrico',
+        'HOST': os.environ.get('JUNTAGRICO_DATABASE_HOST', 'localhost'),
+        # 'localhost', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': os.environ.get('JUNTAGRICO_DATABASE_PORT', '5432'),  # ''', # Set to empty string for default.
         # 'ENGINE': os.environ.get('JUNTAGRICO_DATABASE_ENGINE','django.db.backends.sqlite3'),
         # 'NAME': os.environ.get('JUNTAGRICO_DATABASE_NAME','solila.db'),
         # 'USER': os.environ.get('JUNTAGRICO_DATABASE_USER'), #''junatagrico', # The following settings are not used with sqlite3:
@@ -50,6 +51,7 @@ DATABASES = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['solila/templates'],
         'OPTIONS': {
             'context_processors': [
                 'django.contrib.auth.context_processors.auth',
@@ -65,13 +67,12 @@ TEMPLATES = [
                 'django.template.loaders.filesystem.Loader',
                 'django.template.loaders.app_directories.Loader',
             ],
-            'debug' : True
+            'debug': True
         },
     },
 ]
 
 WSGI_APPLICATION = 'solila.wsgi.application'
-
 
 LANGUAGE_CODE = 'de'
 
@@ -85,13 +86,12 @@ USE_I18N = True
 # calendars according to the current locale.
 USE_L10N = True
 
-DATE_INPUT_FORMATS =['%d.%m.%Y',]
+DATE_INPUT_FORMATS = ['%d.%m.%Y', ]
 
 AUTHENTICATION_BACKENDS = (
     'juntagrico.util.auth.AuthenticateWithEmail',
     'django.contrib.auth.backends.ModelBackend'
 )
-
 
 MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
@@ -105,13 +105,14 @@ MIDDLEWARE = [
 EMAIL_HOST = os.environ.get('JUNTAGRICO_EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('JUNTAGRICO_EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('JUNTAGRICO_EMAIL_PASSWORD')
-EMAIL_PORT = int(os.environ.get('JUNTAGRICO_EMAIL_PORT', '25' ))
-EMAIL_USE_TLS = os.environ.get('JUNTAGRICO_EMAIL_TLS', 'True')=='True'
-EMAIL_USE_SSL = os.environ.get('JUNTAGRICO_EMAIL_SSL', 'False')=='True'
+EMAIL_PORT = int(os.environ.get('JUNTAGRICO_EMAIL_PORT', '25'))
+EMAIL_USE_TLS = os.environ.get('JUNTAGRICO_EMAIL_TLS', 'True') == 'True'
+EMAIL_USE_SSL = os.environ.get('JUNTAGRICO_EMAIL_SSL', 'False') == 'True'
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 
 WHITELIST_EMAILS = []
+
 
 def whitelist_email_from_env(var_env_name):
     email = os.environ.get(var_env_name)
@@ -123,8 +124,6 @@ if DEBUG is True:
     for key in os.environ.keys():
         if key.startswith("JUNTAGRICO_EMAIL_WHITELISTED"):
             whitelist_email_from_env(key)
-            
-
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
@@ -158,42 +157,42 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 VOCABULARY = {
     'member': 'Mitglied',
-    'member_pl' : 'Mitglieder',
-    'assignment' : 'Arbeitseinsatz',
-    'assignment_pl' : 'Arbeitseinsätze',
-    'share' : 'Anteilschein',
-    'share_pl' : 'Anteilscheine',
-    'subscription' : 'Abo',
-    'subscription_pl' : 'Abos',
-    'co_member' : 'Mitabonnent',
-    'co_member_pl' : 'Mitabonnenten',
-    'price' : 'Betriebsbeitrag',
-    'member_type' : 'Mitglied',
-    'member_type_pl' : 'Mitglieder',
-    'depot' : 'Depot',
-    'depot_pl' : 'Depots'
+    'member_pl': 'Mitglieder',
+    'assignment': 'Arbeitseinsatz',
+    'assignment_pl': 'Arbeitseinsätze',
+    'share': 'Anteilschein',
+    'share_pl': 'Anteilscheine',
+    'subscription': 'Abo',
+    'subscription_pl': 'Abos',
+    'co_member': 'Mitabonnent',
+    'co_member_pl': 'Mitabonnenten',
+    'price': 'Betriebsbeitrag',
+    'member_type': 'Mitglied',
+    'member_type_pl': 'Mitglieder',
+    'depot': 'Depot',
+    'depot_pl': 'Depots'
 }
-ORGANISATION_NAME = "solila"
-ORGANISATION_NAME_CONFIG = {"type" : "Verein",
-    "gender" : "M"}
+ORGANISATION_NAME = "SoliLa"
+ORGANISATION_NAME_CONFIG = {"type": "Genossenschaft",
+                            "gender": "F"}
 ORGANISATION_LONG_NAME = "Genossenschaft SoliLa Eulenhof"
 ORGANISATION_ADDRESS = {"name": "Genossenschaft SoliLa Eulenhof",
-            "street": "Schaufelgasse",
-            "number": "34a",
-            "zip": "4313",
-            "city": "Möhlin"}
-ORGANISATION_PHONE =''
-ORGANISATION_BANK_CONNECTION = {"PC" : "0",
-            "IBAN" : "CH 10 0839 2000 1516 0130 5",
-            "BIC" : "",
-            "NAME" : "Freie Gemeinschaftsbank Genossenschaft",
-            "ESR" : ""}
+                        "street": "Schaufelgasse",
+                        "number": "34a",
+                        "zip": "4313",
+                        "city": "Möhlin"}
+ORGANISATION_PHONE = ''
+ORGANISATION_BANK_CONNECTION = {"PC": "0",
+                                "IBAN": "CH 10 0839 2000 1516 0130 5",
+                                "BIC": "",
+                                "NAME": "Freie Gemeinschaftsbank Genossenschaft",
+                                "ESR": ""}
 INFO_EMAIL = "info@solila-eulenhof.ch"
 SERVER_URL = "www.solila.ch"
 ADMINPORTAL_NAME = "juntagrico.solila-eulenhof"
 ADMINPORTAL_SERVER_URL = "juntagrico.solila-eulenhof.ch"
 BUSINESS_REGULATIONS = ""
-BYLAWS = ""
+BYLAWS = "https://solila.ch/wordpress/wp-content/uploads/2020/10/Statuten_SoliLa_Genossenschaft.pdf"
 MAIL_TEMPLATE = "mails/email.html"
 STYLE_SHEET = "/static/juntagrico/css/personal.css"
 FAVICON = "/static/juntagrico/img/favicono.ico"
@@ -207,20 +206,20 @@ CURRENCY = "CHF"
 ASSIGNMENT_UNIT = "ENTITY"
 PROMOTED_JOB_TYPES = []
 PROMOTED_JOBS_AMOUNT = 2
-DEPOT_LIST_GENERATION_DAYS = [1,2,3,4,5,6,7]	
+DEPOT_LIST_GENERATION_DAYS = [1, 2, 3, 4, 5, 6, 7]
 BILLING = False
-BUSINESS_YEAR_START = {"day":1, "month":1}
+BUSINESS_YEAR_START = {"day": 1, "month": 1}
 BUSINESS_YEAR_CANCELATION_MONTH = 10
 MEMBERSHIP_END_MONTH = 6
 IMAGES = {'status_100': '/static/juntagrico/img/status_100.png',
-            'status_75': '/static/juntagrico/img/status_75.png',
-            'status_50': '/static/juntagrico/img/status_50.png',
-            'status_25': '/static/juntagrico/img/status_25.png',
-            'status_0': '/static/juntagrico/img/status_0.png',
-            'single_full': '/static/juntagrico/img/single_full.png',
-            'single_empty': '/static/juntagrico/img/single_empty.png',
-            'single_core': '/static/juntagrico/img/single_core.png',
-            'core': '/static/juntagrico/img/core.png'}
+          'status_75': '/static/juntagrico/img/status_75.png',
+          'status_50': '/static/juntagrico/img/status_50.png',
+          'status_25': '/static/juntagrico/img/status_25.png',
+          'status_0': '/static/juntagrico/img/status_0.png',
+          'single_full': '/static/juntagrico/img/single_full.png',
+          'single_empty': '/static/juntagrico/img/single_empty.png',
+          'single_core': '/static/juntagrico/img/single_core.png',
+          'core': '/static/juntagrico/img/core.png'}
 GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 
-DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
